2a27f073dc059b23553eb0e2254ea3b51c28444e
CaveJohnson376 <cavejohnson376@gmail.com>
2020-01-29 00:52:54 +0500

    создал файл с библиотекой для серво

---
+++ Open-cv/servocontrol.py
@@ -0,0 +1,13 @@
+import serial
+
+class ServoControl:
+    w = 600
+    h = 400
+    
+    def __init__(self, width, height):
+        w = width
+        h = height
+    def chres(self, width, height):
+        w = width
+        h = height
+    
