2939d4b67fa13c4434879e16b221036afd2447a6
CaveJohnson376 <cavejohnson376@gmail.com>
2020-01-28 11:10:14 +0500

    added serial port reading code

--- face_follower_arduino/face_follower_arduino.ino
+++ face_follower_arduino/face_follower_arduino.ino
@@ -1,14 +1,31 @@
 #include <Servo.h>
 Servo rotX;
 Servo rotY;
+int xpos = 0;
+int ypos = 0;
+String in = "";
+boolean isx = true;
 
 
 void setup() {
   Serial.begin(115200);
-
+  rotX.attach(9);
+  rotY.attach(10);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  while (Serial.available() > 0) {
+      int inChar = Serial.read();
+    if (isDigit(inChar)) {
+      in += (char)inChar;
+    }
+    if (inChar == '_') {
+      Xpos = in.toInt());
+      in = "";
+    }
+    if (inChar == '\n') {
+      Ypos = in.toInt());
+      in = "";
+    }
+  }
 }
